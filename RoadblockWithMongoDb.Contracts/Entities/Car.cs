3771d490bc3f236f3f51f6b363234760fd022204
mayor.sheka <mayor.sheka@ukr.net>
2022-07-04 13:01:18 +0300

    added entities

---
+++ RoadblockWithMongoDb.Contracts/Entities/Car.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace RoadblockWithMongoDb.Contracts.Entities
+{
+    public class Car
+    {
+        public Car()
+        {
+            this.AddedOn = DateTime.UtcNow;
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string VehicleNumber { get; set; }
+
+        public ICollection<Person> Persons { get; set; }
+
+        public DateTime AddedOn { get; set; }
+    }
+}
---
+++ RoadblockWithMongoDb.Contracts/Entities/Person.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RoadblockWithMongoDb.Contracts.Entities
+{
+    public class Person
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Age { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool IsDriver { get; set; }
+    }
+}
