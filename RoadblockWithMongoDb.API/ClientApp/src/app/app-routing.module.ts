4382c4f1d4c4fdc1a7843c2de29f2caefa15e1e4
mayor.sheka <mayor.sheka@ukr.net>
2022-07-11 13:26:14 +0300

    added routes for home and car-list component

--- RoadblockWithMongoDb.API/ClientApp/src/app/app-routing.module.ts
+++ RoadblockWithMongoDb.API/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarListComponent } from './presentation/components/car-components/car-list/car-list.component';
+import { HomeComponent } from './presentation/components/home-component/home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+  { path: 'cars', component: CarListComponent },
+];
+
+const carRoutes: Routes = [
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
